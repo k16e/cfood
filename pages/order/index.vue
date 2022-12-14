b107118eb0364cc4c43ebfe5d2bdff8dd612b349
kb <kb@k16e.co>
2022-12-14 18:35:09 +0100

    Now fething mock products and displaying...

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        Main products view
+        <ul class="grid sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:grid-cols-4">
+            <li v-for="i in products" :key="i.id">
+                <NuxtLink :to="`/order/${i.id}`">
+                    {{ i.title }}
+                </NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
 
