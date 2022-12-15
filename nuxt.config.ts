78f628dacc009e82dbbc75857ceb2de5ba7d137d
kb <kb@k16e.co>
2022-12-15 13:04:32 +0100

    Added .env and reading runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,11 @@ export default defineNuxtConfig({
                 { name: 'description', content: 'c.food is on a mission to make food make sense and taste great while at it.' }
             ]
         }
+    },
+    runtimeConfig: { // configs (key) exposed to server routes
+        currencyKey: process.env.CURRENCY_API_KEY,
+        public: { // Keys that may/can be exposed to public routes
+
+        }
     }
 })
--- server/api/currencyApi.js
+++ server/api/currencyApi.js
@@ -1,6 +1,7 @@
 export default defineEventHandler(async () => {
     const
-        { data } = await $fetch('https://api.currencyapi.com/v3/currencies?apikey=nrhLvxOMP2XMlaQZplSIjTKGiWvbpFSVgSBLnhGC')
+        { currencyKey } = useRuntimeConfig(),
+        { data } = await $fetch(`https://api.currencyapi.com/v3/currencies?apikey=${currencyKey}`)
 
     return data
 })
