578c2287bc969dd50d40d5bcc8ea2bfe9cd84297
kb <kb@k16e.co>
2022-12-30 07:57:45 +0100

    Setup supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode'
+        '@nuxtjs/color-mode',
+        '@nuxtjs/supabase'
     ],
     app: {
         head: {
@@ -14,6 +14,8 @@ export default defineNuxtConfig({
     },
     runtimeConfig: { // configs (key) exposed to server routes
         currencyKey: process.env.CURRENCY_API_KEY,
+        supabaseUrl: process.env.SUPABASE_URL,
+        supabaseKey: process.env.SUPABASE_KEY,
         public: { // Keys that may/can be exposed to public routes
 
         }
