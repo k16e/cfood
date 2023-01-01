f4da3d8c405bf16ec07236d8af3d31f5f52e9afa
kb <kb@k16e.co>
2023-01-01 22:09:50 +0100

    ...Added a cart icon

--- components/HeaderMain.vue
+++ components/HeaderMain.vue
@@ -1,20 +1,32 @@
 <template>
-    <header class="">
-        <NuxtLink to="/">
-            Home
-        </NuxtLink>
-        <NuxtLink to="/products">
-            Products
-        </NuxtLink>
-        <NuxtLink to="/research">
-            Research
-        </NuxtLink>
-        <NuxtLink to="/studio">
-            Studio
-        </NuxtLink>
-        <NuxtLink to="/about">
-            Meet c.food
-        </NuxtLink>
+    <header class="p-3">
+        <div class="flex items-center justify-between">
+            <div class="sr-only md:not-sr-only">
+                <NuxtLink to="/">
+                    Home
+                </NuxtLink>
+                <NuxtLink to="/products">
+                    Products
+                </NuxtLink>
+                <NuxtLink to="/research">
+                    Research
+                </NuxtLink>
+                <NuxtLink to="/studio">
+                    Studio
+                </NuxtLink>
+                <NuxtLink to="/about">
+                    Meet c.food
+                </NuxtLink>
+            </div>
+            <div class="">
+                <NuxtLink to="/cart" class="p-2 px-3.5 flex items-center rounded-full bg-white border border-gray-200">
+                    <Icon name="ri:shopping-cart-fill" size="18" class="mr-1.5 text-orange-700"/>
+                    <span class="">
+                        Cart <span>(0)</span>
+                    </span>
+                </NuxtLink>
+            </div>
+        </div>
     </header>
 </template>
 
