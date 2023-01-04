dedca838385af1848334fe711a2e4cff4118920a
kb <kb@k16e.co>
2023-01-04 09:15:26 +0100

    ...

---
+++ components/FooterMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>
+            &copy; 2022 c.food Studio
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
-    <header class="p-3 fixed inset-x-0 top-0 h-16 bg-white border-b border-gray-200 z-30">
-        <HeaderMain/>
-    </header>
-    <main class="mt-16">
-        <slot/>
-    </main>
+    <div class="flex flex-col min-h-screen">
+        <header class="p-3 fixed inset-x-0 top-0 h-16 bg-white border-b border-gray-200 z-30">
+            <HeaderMain/>
+        </header>
+        <main class="mt-16 flex-1">
+            <slot/>
+        </main>
 
-    <footer>
-        Footer Here
-    </footer>
+        <footer>
+            <FooterMain/>
+        </footer>
+    </div>
 </template>
 
 <script setup>
