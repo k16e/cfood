38476649f487128845e1f438cdb44fe68140d7c4
kb <kb@k16e.co>
2023-01-05 19:26:32 +0100

    Added active link marker

--- components/HeaderMain.vue
+++ components/HeaderMain.vue
@@ -5,12 +5,12 @@
             <NuxtLink to="/" class="flex items-center justify-center rounded-full mr-5">
                 <HeaderLogo/>
             </NuxtLink>
-            <nav class="sr-only md:not-sr-only flex !h-full items-center">
+            <nav class="sr-only md:not-sr-only flex !h-[95%] items-center space-x-3">
                 <h2 v-text="'Main navigation'" class="sr-only"/>
                 <NuxtLink
                     v-for="(item, idx) in mainnav" :key="item.title + idx"
                     :to="item.link"
-                    class="flex items-center h-full px-5 rounded-full focus:bg-orange-50 hover:bg-orange-50">
+                    class="flex items-center h-full px-3 rounded-full focus:bg-orange-50 hover:bg-orange-50">
                     {{ item.title }}
                 </NuxtLink>
             </nav>
