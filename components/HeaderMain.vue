99d21432fa7d36713ef4d902f417de783c7534ea
kb <kb@k16e.co>
2023-01-05 20:34:12 +0100

    ...

--- components/HeaderMain.vue
+++ components/HeaderMain.vue
@@ -7,13 +7,15 @@
             </NuxtLink>
             <nav class="sr-only md:not-sr-only flex !h-[95%] items-center space-x-3">
                 <h2 v-text="'Main navigation'" class="sr-only"/>
-                <NuxtLink
-                    v-for="(route, idx) in routes" :key="route.title + idx"
-                    :to="route.path"
-                    class="flex items-center h-full px-3 rounded-full focus:bg-orange-50 hover:bg-orange-50"
-                    exact>
-                    {{ route.title }}
-                </NuxtLink>
+                <ul class="flex items-center h-full w-full">
+                    <li v-for="(route, idx) in routes" :key="route.title + idx" class="flex items-center h-full">
+                        <NuxtLink
+                            :to="route.path"
+                            class="flex items-center h-full px-3 rounded-full focus:bg-orange-50 hover:bg-orange-50">
+                            {{ route.title }}
+                        </NuxtLink>
+                    </li>
+                </ul>
             </nav>
         </div>
         <div class="flex items-center space-x-1.5">
@@ -30,4 +32,5 @@ const routes = [
     { title: 'Studio', path: '/studio' },
     { title: 'Meet c.food', path: '/about' }
 ]
+const route = useRoute()
 </script>
