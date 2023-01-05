4c8feae2ed612a3b605d779f2f898037d8831aa5
kb <kb@k16e.co>
2023-01-05 07:53:48 +0100

    Added c.food logo as image to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,29 @@
 <template>
     <Container padX center>
-        <div class="pt-7 lg:pt-12 pb-7 sm:pb-9 lg:pb-12">
-            <div class="pb-5 sm:pb-7">
-                <Heading tag="h1" content="Welcome to c.food!"/>
+        <div class="grid md:grid-cols-3 items-center gap-12 sm:gap-14 pt-7 lg:pt-12">
+            <div class="md:col-span-2">
+                <div class="pb-5 sm:pb-7">
+                    <Heading tag="h1" content="Welcome to c.food!"/>
+                </div>
+                <div class="prose prose-xl lg:prose-2xl leading-normal lg:leading-snug italic max-w-none">
+                    <p>
+                        This year, make it a goal to eat food that expires, even quickly, that you can pronounce, that your grandmother wouldn't pass up.
+                    </p>
+                    <p>
+                        <NuxtLink to="/studio">At c.food</NuxtLink>, we're proud to be making such foods. Thanks to the modern invention of the refrigerator, most of them do keep up to a week. Some even longer! But all of them preservatives and funny-sounding chemicals free! Only fresh, refrigerated goodnesses.
+                    </p>
+                </div>
+            <div class="pt-12 sm:pt-14">
+                <NuxtLink to="/products" class="luna-btn text-gray-600 font-medium">
+                    <Icon name="ic:baseline-shopping-basket" size="22" class="mr-1.5 text-orange-700"/>
+                    <span>Shop our products</span>
+                </NuxtLink>
             </div>
-            <div class="prose prose-xl lg:prose-2xl leading-normal lg:leading-snug italic max-w-screen-lg">
-                <p>
-                    This year, make it a goal to eat food that expires, even quickly, that you can pronounce, that your grandmother wouldn't pass up.
-                </p>
-                <p>
-                    <NuxtLink to="/studio">At c.food</NuxtLink>, we're proud to be making such foods. Thanks to the modern invention of the refrigerator, most of them do keep up to a week. Some even longer! But all of them preservatives and funny-sounding chemicals free! Only fresh, refrigerated goodnesses.
-                </p>
             </div>
+            <figure>
+                <img src="/logo.png" alt="">
+            </figure>
         </div>
-        <NuxtLink to="/products" class="luna-btn text-gray-600 font-medium">
-            <Icon name="ic:baseline-shopping-basket" size="22" class="mr-1.5 text-orange-700"/>
-            <span>Shop our products</span>
-        </NuxtLink>
     </Container>
 </template>
 
