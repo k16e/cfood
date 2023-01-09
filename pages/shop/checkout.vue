2cef8bbeb2ca03d2c92f3b99d275350e353a3ce6
kb <kb@k16e.co>
2023-01-09 13:58:20 +0100

    Added little spinner animation

--- pages/shop/checkout.vue
+++ pages/shop/checkout.vue
@@ -90,8 +90,14 @@
 
                                 <button
                                     type="submit"
-                                    class="mt-9 w-full rounded-md border border-transparent bg-orange-700 py-3 px-4 font-medium text-white shadow-sm hover:bg-orange-600">
-                                    Proceed to pay
+                                    class="mt-9 luna-btn _is-block"
+                                    :class="{ 'cursor-not-allowed': formActions.sending.value }"
+                                    :disabled="formActions.sending.value">
+                                    <svg v-if="formActions.sending.value" class="mr-3 h-5 w-5 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                    <span v-text="'Proceed to pay'"/>
                                 </button>
                             </form>
                         </section>
@@ -119,11 +125,20 @@ const customer = {
     phone: ref(''),
     address: ref('')
 }
+let formActions = {
+    sent: ref(false),
+    sending: ref(false),
+    error: ref(false),
+    errorMessage: ref(null)
+}
+console.log(formActions.sending.value)
 const order = store.order
 
 const router = useRouter()
 const proceedToPay = async () => {
     const supabase = useSupabaseClient()
+
+    formActions.sending.value = true
     order.push({
         customer_name: customer.customer_name.value,
         email: customer.email.value,
@@ -159,12 +174,14 @@ const proceedToPay = async () => {
                 }
             ], { onConflict: 'email', ignoreDuplicates: false })
 
+        formActions.sending.value = false
         if (order_error) throw order_error
         if (customer_error) throw customer_error
 
     } catch (err) {
         console.log(err)
     } finally {
+        formActions.sent.value = true
         store.$patch({ cart: [], order: [] })
         router.push({ path: '/shop/success' })
         setTimeout(() => {
