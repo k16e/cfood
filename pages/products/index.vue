3bf684a436c547190c993a6a86e930759bd70457
kb <kb@k16e.co>
2023-01-10 08:01:00 +0100

    ...

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,27 +1,29 @@
 <template>
-    <Container center padX>
-        <div class="flex items-center justify-between">
-            <Heading tag="h1" content="Our products"/>
-        </div>
-        <ul class="pt-5 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-5">
-            <li v-for="product in products" :key="product.uuid">
-                <ProductsCard :product="product"/>
-            </li>
-        </ul>
-    </Container>
+    <WrapperReveal>
+        <Container center padX>
+            <div class="flex items-center justify-between">
+                <Heading tag="h1" content="Our products"/>
+            </div>
+            <ul class="pt-5 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-5">
+                <li v-for="product in products" :key="product.uuid">
+                    <ProductsCard :product="product"/>
+                </li>
+            </ul>
+        </Container>
+    </WrapperReveal>
 </template>
 
 <script setup>
-    import { useProductsStore } from '~/stores/products'
+import { useProductsStore } from '~/stores/products'
 
-    const store = useProductsStore()
-    await store.fetchProducts()
-    const products = await store.products
+const store = useProductsStore()
+await store.fetchProducts()
+const products = await store.products
 
-    useHead({
-        title: 'c.food Products (All Condiments & foodstuff)',
-        meta: [
-            { name: 'description', content: 'All condiments and foodstuff from c.food' }
-        ]
-    })
+useHead({
+    title: 'c.food Products (All Condiments & foodstuff)',
+    meta: [
+        { name: 'description', content: 'All condiments and foodstuff from c.food' }
+    ]
+})
 </script>
