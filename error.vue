9ba0588134ac35c5c1ae5436552ff8e189f2fd71
kb <kb@k16e.co>
2023-01-14 17:00:02 +0100

    ...

--- error.vue
+++ error.vue
@@ -7,17 +7,12 @@
             {{ error.message }}
         </p>
 
-        <!-- Button to clear the error -->
-        <button @click="handleError" class="">Please go back home</button>
+        <a href="/">Please go back home</a>
     </div>
 </template>
 
 <script setup>
     const props = defineProps({ error: Object })
-    // customise 404 message from script section
     const error = useError()
-    if (error.statusCode === 404 || error.statusCode === 500) {
-        error.value.message = 'Oops! Page not found 😔';
-    }
-    const handleError = () => clearError({ redirect: '/' })
+    error.value.message = 'Oops! Page not found 😔'
 </script>
--- pages/[id].vue
+++ pages/[id].vue
@@ -15,4 +15,14 @@ const story = await useAsyncStoryblok(`pages${route.path}`, {
     version: config.storyblokVersion,
     resolve_links: 'story'
 })
+// let story = ref(null)
+// try {
+//     story = await useAsyncStoryblok(`pages${route.path}`, {
+//         version: config.storyblokVersion,
+//         resolve_links: 'story'
+//     })
+// } catch (error) {
+//     console.log(error)
+//     throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+// }
 </script>
