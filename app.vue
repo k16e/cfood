85656a8904253c22109c517828876b042451a369
kb <kb@k16e.co>
2023-01-14 16:12:04 +0100

    Switched to explicit app.vue, ditched layouts

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <NuxtLoadingIndicator :color="`#ea580c`"/>
+    <NuxtLayout>
+        <div class="flex flex-col min-h-screen">
+            <header class="p-3 fixed inset-x-0 top-0 h-16 bg-white border-b border-gray-200 z-30">
+                <HeaderMain/>
+            </header>
+            <main class="mt-16 flex-1 py-7 lg:py-12">
+                <NuxtPage/>
+            </main>
+            <footer class="py-1.5">
+                <FooterMain/>
+            </footer>
+        </div>
+    </NuxtLayout>
+</template>
+
+<script setup>
+
+</script>
