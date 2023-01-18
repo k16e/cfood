d29b8c7cdca88572ad80ada01014263f0cae6d48
kb <kb@k16e.co>
2023-01-18 18:20:53 +0100

    Added more Waves and improved them placements

--- components/Wave.vue
+++ components/Wave.vue
@@ -1,14 +1,21 @@
 <template>
-    <svg viewBox="0 0 1600 800" width="100%" preserveAspectRatio="none">
+    <svg
+        viewBox="0 0 1600 800"
+        width="100%"
+        preserveAspectRatio="none"
+        class="scale-x-[-1] inset-x-0">
         <path
-            fill="#fff"
-            stroke="#d1d5db"
-            stroke-width="4"
+            :fill="fill"
+            :stroke="stroke && '#d1d5db'"
+            :stroke-width="stroke && '4'"
             d="M478.4 581c3.2 0.8 6.4 1.7 9.5 2.5c196.2 52.5 388.7 133.5 593.5 176.6c174.2 36.6 349.5 29.2 518.6-10.2V0H0v574.9c52.3-17.6 106.5-27.7 161.1-30.9C268.4 537.4 375.7 554.2 478.4 581z"
         />
     </svg>
 </template>
 
 <script setup>
-
+const props = defineProps({
+    fill: { type: String, default: '#fff' },
+    stroke: { type: Boolean, default: true }
+})
 </script>
--- storyblok/PromoTiles.vue
+++ storyblok/PromoTiles.vue
@@ -2,10 +2,20 @@
     <WrapperReveal>
         <WrapperSection>
             <div class="overflow-hidden pt-32 sm:pt-16 z-[1] relative">
+                <Wave
+                    class="absolute z-[1] max-h-[96px] top-0"
+                    :stroke="false"
+                    :fill="'#f2f2f2'"
+                />
+                <Wave
+                    class="absolute z-[3] max-h-[96px] bottom-0 rotate-180 scale-x-[1]"
+                    :stroke="false"
+                    :fill="'#f2f2f2'"
+                />
                 <div class="bg-gradient-to-r from-orange-500 to-orange-800">
                     <Container center padX>
-                        <div class="relative min-h-[424px] lg:min-h-[480px] flex items-center pt-32 sm:pt-0">
-                            <div class="text-center flex flex-col items-center w-full sm:text-left sm:block">
+                        <div class="relative min-h-[480px] sm:min-h-[528px] md:min-h-[640px] flex items-center">
+                            <div class="text-center flex flex-col items-center w-full sm:text-left sm:block sm:-translate-y-7">
                                 <Heading tag="h2" font="font-sans" color="text-white" weight="font-bold"
                                     class="text-4xl md:text-5xl lg:text-6xl !leading-none">
                                     <span v-text="blok.heading[0].line_1" class="block"/>
@@ -20,7 +30,7 @@
                                 </div>
                             </div>
 
-                            <div class="absolute -top-24 left-1/2 -translate-x-1/2 transform sm:top-7 sm:translate-x-0">
+                            <div class="absolute -top-24 left-1/2 -translate-x-1/2 transform sm:top-7 sm:translate-x-0 z-[2]">
                                 <div class="ml-24 flex min-w-max space-x-5 sm:ml-3 lg:space-x-7">
                                     <div class="flex space-x-5 sm:flex-col sm:space-x-0 sm:space-y-5 lg:space-y-7">
                                         <NuxtImg
