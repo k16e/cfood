c64a4a16c45c324569eb17d6cc40131749944151
kb <kb@k16e.co>
2023-01-28 11:08:30 +0100

    Set cart length as computed prop in CartLink

--- components/products/CartLink.vue
+++ components/products/CartLink.vue
@@ -5,13 +5,12 @@
         <Icon name="ri:shopping-cart-fill" size="22" class="mr-1.5 text-orange-700"/>
         <span class="flex items-center">
             <span v-text="`Cart`" class="hidden lg:block mr-1"/>
-            <ClientOnly>
-                <sup v-text="`(${cart.length})`" class="font-medium text-orange-700"/>
-            </ClientOnly>
+            <sup v-text="`(${cartLength})`" class="font-medium text-orange-700"/>
         </span>
     </NuxtLink>
 </template>
 
 <script setup>
-const { cart } = useProductsStore()
+const productStore = useProductsStore()
+const cartLength = computed(() => productStore.cart.length)
 </script>
