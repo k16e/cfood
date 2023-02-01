16af45eff82274dfb7a68e9539d68afc19548914
kb <kb@k16e.co>
2023-02-01 22:08:17 +0100

    ...

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -1,13 +1,11 @@
 <template>
-    <ClientOnly>
-        <Transition name="fade-in">
-            <div
-                @click.self="app.removeOverlay"
-                v-if="app.overlay"
-                class="fixed inset-0 bg-black bg-opacity-70 ease duration-300 transition-opacity"
-            />
-        </Transition>
-    </ClientOnly>
+    <Transition name="fade-in">
+        <div
+            @click.self="app.removeOverlay"
+            v-if="app.overlay"
+            class="fixed inset-0 bg-black bg-opacity-70 ease duration-300 transition-opacity"
+        />
+    </Transition>
 </template>
 
 <script setup>
