92974e3d2f53c2ca8ddfb4599c3c7d5c774654bd
kb <kb@k16e.co>
2023-02-01 22:25:02 +0100

    Nested transition is not the cause of lag in prod

--- components/Sheet.vue
+++ components/Sheet.vue
@@ -4,10 +4,9 @@
             v-if="app.sheet"
             class="fixed bottom-0 inset-x-0 bg-white min-h-[150px]">
             <Wave top="top-[-24px]" max-height="max-h-[64px]" :stroke="false" :fill="`#fff`" flip-forward/>
-            <!-- <Transition name="flip">
+            <Transition name="flip">
                 <NavMobile v-if="app.navMobile"/>
-            </Transition> -->
-            <NavMobile v-if="app.navMobile"/>
+            </Transition>
         </div>
     </Transition>
 </template>
