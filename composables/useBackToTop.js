eb493f93269582bdb79bb07cdde9ee9d1be030b8
kb <kb@k16e.co>
2023-02-05 19:00:24 +0100

    Refactored back to top into own composable

---
+++ composables/useBackToTop.js
@@ -0,0 +1,26 @@
+const backToTop = (app) => {
+    const footer = document.getElementById('footer')
+    const footerHeight = Math.ceil(footer.getBoundingClientRect().height)
+    const root = document.documentElement
+    const footerVisible = footer.getAttribute('data-visible')
+    let divisor
+
+    if (window.pageYOffset > 640) {
+        app.showBackToTop = true
+        const backToTop = document.getElementById('back-to-top')
+        const bttHeigt = backToTop?.getBoundingClientRect()?.height || 56
+        divisor = Math.round(bttHeigt)
+    }
+    else app.showBackToTop = false
+
+    if (footerVisible === 'true') {
+        root.style.setProperty('--back-to-top-translate', ((footerHeight * -1) + divisor) + 'px')
+    }
+    else root.style.setProperty('--back-to-top-translate', 0)
+}
+
+const useBackToTop = () => {
+    return { backToTop }
+}
+
+export default useBackToTop
