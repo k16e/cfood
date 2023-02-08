bdb2217ac772927830901090ba2572a534af02fa
kb <kb@k16e.co>
2023-02-08 03:02:17 +0100

    ...

--- components/products/UserMenu.vue
+++ components/products/UserMenu.vue
@@ -4,7 +4,7 @@
         class="luna-btn _is-padless border-none relative">
         <Icon name="ri:user-smile-fill" size="32" class="text-orange-700"/>
         <span class="flex items-center">
-            <span v-text="`Hey, me!`" class="hidden lg:block ml-1"/>
+            <span v-text="`Your account`" class="hidden lg:block ml-1"/>
             <Icon name="ic:baseline-arrow-drop-down" size="22" class="-mr-1.5 text-orange-700"/>
         </span>
     </button>
