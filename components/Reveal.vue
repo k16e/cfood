f1ae4ba2b79abcceeca3bf7ace74f08fda466c05
kb <kb@k16e.co>
2023-02-10 18:26:38 +0100

    Refactored out Reveal's CSS

--- components/Reveal.vue
+++ components/Reveal.vue
@@ -19,3 +19,9 @@ onUpdated(() => stop())
 onBeforeRouteLeave(() => el.value.classList.add('_on-route-change'))
 onBeforeRouteUpdate(() => el.value.classList.add('_on-route-change'))
 </script>
+
+<style scoped>
+.luna-reveal { @apply scale-110 opacity-0 invisible transition duration-700; }
+.luna-reveal._is-in-viewport { @apply scale-100 opacity-100 visible; }
+.luna-reveal._on-route-change { @apply scale-110 opacity-0 transition duration-700; }
+</style>
