48923e87f02d65cadf4441be108af5e83b65ee94
kb <kb@k16e.co>
2023-02-11 18:31:54 +0100

    Yes, SB throws create error on error, only reason for t/c is to be able to show loading animation, which actually works, but not displaying on instant of window load...

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,10 @@
 <template>
-    <Reveal v-if="loading" class="fixed top-28 left-1/2 -translate-x-1/2">
+    <div v-if="loading" class="fixed top-28 left-1/2 -translate-x-1/2">
         <svg class="h-8 w-8 text-orange-700 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
-    </Reveal>
+    </div>
     <StoryblokComponent v-else :blok="content.content"/>
 </template>
 
@@ -17,11 +17,10 @@ const loading = ref(true)
 const story = async () => {
     let content
     try {
-        await useAsyncStoryblok(path, {
+        content = await useAsyncStoryblok(path, {
             version: config.storyblokVersion,
             resolve_links: 'story'
         })
-        .then(result => content = result.value)
     } catch (error) { console.error() }
     finally { loading.value = false }
     return content
