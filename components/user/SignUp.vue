d74c59ce0414125f84af586aea2f36cb46d690bb
kb <kb@k16e.co>
2023-02-12 17:59:49 +0100

    Added sign up page, and the switcher

--- components/user/SignUp.vue
+++ components/user/SignUp.vue
@@ -3,7 +3,7 @@
         <div class="flex min-h-full items-center justify-center px-4 sm:px-6 lg:px-8">
             <div class="w-full max-w-md space-y-8">
                 <PageHeader
-                    tag="h1" content="Sign in to your account!"
+                    tag="h1" content="Get a c.food account!"
                     link="" linkIcon=""
                 />
                 <form
@@ -11,17 +11,13 @@
                     @submit.prevent="handleSubmit">
                     <div class="grid grid-cols-1 gap-5 sm:gap-7 mb-3">
                         <p>
-                            <label for="email" class="luna-label">Email:</label>
+                            <label for="email" class="luna-label">Enter email:</label>
                             <input v-model="customer.email.value" type="email" id="email" autocomplete="email" required placeholder="Email address" class="luna-input"/>
                         </p>
                         <p>
-                            <label for="password" class="luna-label">Password:</label>
+                            <label for="password" class="luna-label">Choose password:</label>
                             <input v-model="customer.password.value" type="password" id="password" autocomplete="current-password" required placeholder="Password" class="luna-input"/>
                         </p>
-                    </div>
-
-                    <div class="text-sm">
-                        <a href="#" class="font-medium text-orange-700 hover:text-orange-500">Forgot your password?</a>
                     </div>
 
                     <div class="mt-9">
@@ -31,14 +27,14 @@
                             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                                 <Icon name="ic:round-lock-person" class="h-5 w-5 text-orange-400 group-hover:text-orange-100" aria-hidden="true" />
                             </span>
-                            Sign in
+                            Create an account
                         </button>
                     </div>
                 </form>
 
                 <p class="text-sm text-center">
-                    <NuxtLink to="/customer/sign-up" class="font-medium text-orange-700 hover:text-orange-500 p-3 rounded-full">
-                        Get a new account with c.food Studio!
+                    <NuxtLink to="/customer/sign-in" class="font-medium text-orange-700 hover:text-orange-500 p-3 rounded-full">
+                        Have an account? Sign in instead!
                     </NuxtLink>
                 </p>
             </div>
---
+++ pages/customer/sign-up.vue
@@ -0,0 +1,10 @@
+<template>
+    <Reveal class="pb-24 sm:pb-28">
+        <UserSignUp/>
+    </Reveal>
+    <SiteFooter/>
+</template>
+
+<script setup>
+
+</script>
