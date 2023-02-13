e083d4171888bd696164c5ccb51943b776de86bf
kb <kb@k16e.co>
2023-02-13 18:58:26 +0100

    ...

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ export default defineNuxtRouteMiddleware((to, _from) => {
     const user = useSupabaseUser()
 
     if (!user.value) {
-        return navigateTo('/customer/sign-in')
+        setTimeout(() => { // Temporary fix to Reveal not losing its _on-route-change class...
+            return navigateTo('/customer/sign-in')
+        }, 100)
     }
 })
