06bfd33dcd89be8be814c4f7f74788af67c403c8
kb <kb@k16e.co>
2023-02-14 00:56:09 +0100

    Improve user account link with swapping based on user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,5 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
     const user = useSupabaseUser()
 
-    if (!user.value) {
-        setTimeout(() => { // Temporary fix to Reveal not losing its _on-route-change class...
-            return navigateTo('/customer/sign-in')
-        }, 100)
-    }
+    if (!user.value) return navigateTo('/customer/sign-in')
 })
